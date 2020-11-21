db534a97d92fbbe6c2d1459364665184fd94514a
signalm <73932731+signalm@users.noreply.github.com>
2020-11-22 03:30:27 +0800

    Update TV.js

--- TV.js
+++ TV.js
@@ -1,2 +1,2 @@
-body = $response.body.replace(/\"pro\_plan\"\:\s\"\"/, "\"isPremium\":true")
+body = $response.body.replace(/\"pro\_plan\"\:\s\"\"/, "\"pro\_plan\"\:\s\"pro\_premium\_trial\"")
 $done({body});
