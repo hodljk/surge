fd339b32470d616e9300d11ce3a7544b92d2d398
signalm <73932731+signalm@users.noreply.github.com>
2020-11-22 03:54:49 +0800

    Update TV.js

--- TV.js
+++ TV.js
@@ -1,2 +1,5 @@
-body = $response.body.replace(/\"pro\_plan\"\:\s\"\"/, "\"pro\_plan\"\:\s\"pro\_premium\_trial\"")
+var body = $response.body;
+let obj = JSON.parse(body);
+obj.pro_plan = pro_premium_trial
+body = JSON.stringify(obj);
 $done({body});
