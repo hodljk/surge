99012fbd3f5d1b158777d08b864db67048e828ea
signalm <73932731+signalm@users.noreply.github.com>
2020-11-22 04:55:25 +0800

    Update TradingView.js

--- TradingView.js
+++ TradingView.js
@@ -1,5 +1,13 @@
 let obj = JSON.parse($response.body);
+obj.socials["twitter"] = "";
+obj.location = "";
 obj.pro_plan = "pro_premium_trial";
-obj.badgets["name"] = "pro:pro_premium_trial";
-obj.badgets["verbose_name"] = "Premium试用";
+obj.status = "";
+obj.following_count = 0;
+obj.follow_status = false;
+obj.is_online = false;
+obj.avatars.["mid"] = "";
+obj.avatars.["big"] = "";
+obj.avatars.["small"] = "";
+obj.avatars.["orig"] = null;
 $done({body: JSON.stringify(obj)});
