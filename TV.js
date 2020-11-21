e6a12f7db5a7c72739290e2d39219c658159de48
signalm <73932731+signalm@users.noreply.github.com>
2020-11-22 04:11:05 +0800

    Update TV.js

--- TV.js
+++ TV.js
@@ -1,6 +1,5 @@
 var body = $response.body;
 let obj = JSON.parse(body);
-obj.pro_plan = pro_premium_trial;
-obj.is_authenticated = true;
+obj = {"pro_plan":"pro_premium_trial"
 body = JSON.stringify(obj);
 $done({body});
