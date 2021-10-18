aa5c45c420cd6dbb8030757169b5f499054aa198
signalm <73932731+signalm@users.noreply.github.com>
2021-10-19 02:54:23 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -8,13 +8,6 @@
   let total = info.total;
   let expire = args.expire || info.expire;
   let content = [`Usage：${bytesToSize(used)} | ${bytesToSize(total)}`];
-
-  if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天`);
-  }
-  if (expire) {
-    if (/^[\d]+$/.test(expire)) expire *= 1000;
-    content.push(`到期：${formatTime(expire)}`);
   }
 
   let now = new Date();
